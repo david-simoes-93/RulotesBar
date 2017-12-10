f885e7ace5ec365f7be9c7576a24940acd8d4d32
bluemoon93 <david.simoes@ua.pt>
2017-12-10 17:07:39 +0000

    English evrsion

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 @app.route('/email', methods=['POST'])
 def send_email():
+    # https://stackoverflow.com/questions/8329741/issue-with-smtplib-sending-mail-with-unicode-characters-in-python-3-1
 	destination = 'gamecloner@gmail.com'
 	#print("lol",request.form["email"],
 	#request.form["name"],
@@ -25,10 +26,36 @@ def send_email():
 	   smtpObj.sendmail(sender, receivers, message)
 	   print("Successfully sent email")
 	   return render_template('email.html')
-	except smtplib.SMTPException:
+	except:
 	   print("Error: unable to send email")
 
 	return render_template('email_error.html')
+
+
+@app.route('/email_en', methods=['POST'])
+def send_email_en():
+	destination = 'gamecloner@gmail.com'
+	#print("lol",request.form["email"],
+	#request.form["name"],
+	#request.form["message"],
+	#request.form["subject"])
+
+	sender = request.form["email"]
+	receivers = [destination]
+
+	message = "CC: "+request.form["name"]+" <"+request.form["email"]+">\nTo: RulotesBar <"+destination+\
+	    ">\nSubject: "+request.form["subject"]+"\n"+request.form["message"]
+	print(message)
+
+	try:
+	   smtpObj = smtplib.SMTP('localhost')
+	   smtpObj.sendmail(sender, receivers, message)
+	   print("Successfully sent email")
+	   return render_template('email_en.html')
+	except:
+	   print("Error: unable to send email")
+
+	return render_template('email_error_en.html')
 
 
 @app.route('/')
@@ -36,6 +63,11 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/en')
+def index_en():
+    return render_template('index_en.html')
+
+
 if __name__ == "__main__":
-    app.run(host='192.168.1.64',port=5000)
+    app.run(host='127.0.0.1',port=5000)
 
