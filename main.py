8b4782b23997cbe4678da83a34aa365169d32175
bluemoon93 <david.simoes@ua.pt>
2017-12-13 20:35:09 +0000

    emails

--- main.py
+++ main.py
@@ -9,10 +9,9 @@ app = Flask(__name__)
 
 @app.route('/email', methods=['POST'])
 def send_email():
-	destination = 'gamecloner@gmail.com'
-
-	sender = request.form["email"]
-	receivers = [destination]
+	myemail = 'gamecloner@gmail.com'
+	client = request.form["email"]
+	#receivers = [myemail]
 
 	message2 = "To: "+Header(request.form["name"], "utf-8").encode()+" <"+request.form["email"]+\
 	    ">\nSubject: "+Header(request.form["subject"], "utf-8").encode()+"\n"+MIMEText(
@@ -21,15 +20,15 @@ def send_email():
 	    "Obrigado pela sua atenção,\nRulotesBar", _charset="UTF-8").as_string()
 
 	message = "CC: "+Header(request.form["name"], "utf-8").encode()+" <"+request.form["email"]+\
-		">\nTo: RulotesBar <"+destination+\
+		">\nTo: RulotesBar <"+myemail+\
 	    ">\nSubject: "+Header(request.form["subject"], "utf-8").encode()+"\n"+\
 	    MIMEText(request.form["message"], _charset="UTF-8").as_string()
 	print(message)
 
 	try:
 	   smtpObj = smtplib.SMTP('localhost')
-	   smtpObj.sendmail(sender, receivers, message)
-	   smtpObj.sendmail(destination, sender, message2)
+	   smtpObj.sendmail("rulotesbar@gmail.com", myemail, message)
+	   smtpObj.sendmail("rulotesbar@gmail.com", client, message2)
 	   print("Successfully sent email")
 	   return render_template('email.html')
 	except:
@@ -40,10 +39,9 @@ def send_email():
 
 @app.route('/email_en', methods=['POST'])
 def send_email_en():
-	destination = 'gamecloner@gmail.com'
-
-	sender = request.form["email"]
-	receivers = [destination]
+	myemail = 'gamecloner@gmail.com'
+	client = request.form["email"]
+	#receivers = [myemail]
 
 	message2 = "To: "+Header(request.form["name"], "utf-8").encode()+" <"+request.form["email"]+\
 	    ">\nSubject: "+Header(request.form["subject"], "utf-8").encode()+"\nThis is an automated message.\n\n"+\
@@ -51,15 +49,15 @@ def send_email_en():
 	    "Thank you for your attention,\nRulotesBar"
 
 	message = "CC: "+Header(request.form["name"], "utf-8").encode()+" <"+request.form["email"]+\
-		">\nTo: RulotesBar <"+destination+\
+		">\nTo: RulotesBar <"+myemail+\
 	    ">\nSubject: "+Header(request.form["subject"], "utf-8").encode()+"\n"+\
 	    MIMEText(request.form["message"], _charset="UTF-8").as_string()
 	print(message)
 
 	try:
 	   smtpObj = smtplib.SMTP('localhost')
-	   smtpObj.sendmail(sender, receivers, message)
-	   smtpObj.sendmail(destination, sender, message2)
+	   smtpObj.sendmail("rulotesbar@gmail.com", myemail, message)
+	   smtpObj.sendmail("rulotesbar@gmail.com", client, message2)
 	   print("Successfully sent email")
 	   return render_template('email_en.html')
 	except:
